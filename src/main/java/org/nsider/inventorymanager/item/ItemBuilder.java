1b12563b3723950eb1da60b286f8b447053ec99a
nsider4 <115684594+nsider4@users.noreply.github.com>
2023-08-13 17:32:16 -0400

    Update ItemBuilder.java

--- src/main/java/org/nsider/inventorymanager/item/ItemBuilder.java
+++ src/main/java/org/nsider/inventorymanager/item/ItemBuilder.java
@@ -1,17 +1,66 @@
 package org.nsider.inventorymanager.item;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.nsider.inventorymanager.inventory.InventoryBuilder;
 import org.nsider.inventorymanager.inventory.InventoryHandler;
 import org.nsider.inventorymanager.utils.MaterialUtils;
 
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ItemBuilder {
+
+    private static final Map<Long, ItemStack> idToItemMap = new IdentityHashMap<>();
+    private static final AtomicLong nextId = new AtomicLong(0);
+
+    /**
+     * Gets or generates an ID for the given ItemStack.
+     *
+     * @param item The ItemStack for which to get or generate an ID.
+     * @return The ID associated with the item.
+     */
+    public static long getIdFor(ItemStack item) {
+        for (Map.Entry<Long, ItemStack> entry : idToItemMap.entrySet()) {
+            if (entry.getValue().equals(item)) {
+                return entry.getKey(); // Return the existing ID if item is found
+            }
+        }
+
+        long id = nextId.getAndIncrement(); // Generate a new ID
+        idToItemMap.put(id, item); // Associate the item with the new ID
+        return id; // Return the new ID
+    }
+
+    /**
+     * Removes an item with the specified ID from the mapping.
+     *
+     * @param id The ID of the item to be removed.
+     */
+    public static void removeItem(long id) {
+        idToItemMap.remove(id); // Remove the item associated with the given ID
+    }
+
+    /**
+     * Returns an unmodifiable view of the mapping containing all items and their IDs.
+     *
+     * @return Unmodifiable map containing all items and their IDs.
+     */
+    public static Map<Long, ItemStack> getAllItems() {
+        return Collections.unmodifiableMap(idToItemMap); // Return an unmodifiable view of the item map
+    }
+
+    /**
+     * Retrieves the ItemStack associated with the given ID.
+     *
+     * @param id The ID of the ItemStack to be retrieved.
+     * @return The ItemStack associated with the provided ID.
+     */
+    public static ItemStack getItemForId(long id) {
+        return idToItemMap.get(id); // Retrieve the item associated with the given ID
+    }
+
 
     /**
      * Creates an ItemStack from the given ItemData.
@@ -22,21 +71,25 @@ public class ItemBuilder {
      */
     public static ItemStack fromItemData(ItemData info, Material... forcedMaterial) {
         if (info.getMaterial().contains("head-")) {
-            return MaterialUtils.addMeta(
+            ItemStack item = MaterialUtils.addMeta(
                     MaterialUtils.getCustomTextureHead(
                             info.getMaterial().split("-")[1]),
                             info.getAmount(),
                             info.getName(),
                             info.getLore()
             );
+            info.setID(getIdFor(item));
+            return item;
         } else {
             Material material = forcedMaterial.length == 0 ? MaterialUtils.getMaterial(info.getMaterial()) : forcedMaterial[0];
-            return MaterialUtils.createItem(
+            ItemStack item = MaterialUtils.createItem(
                     material,
                     info.getAmount(),
                     info.getName(),
                     info.getLore()
             );
+            info.setID(getIdFor(item));
+            return item;
         }
     }
 
@@ -53,16 +106,16 @@ public class ItemBuilder {
             ConcurrentHashMap<ItemStack, List<Integer>> itemsMap = builder.getItemsMap();
 
             if (itemsMap.containsKey(item)) {
-                List<Integer> slots = itemsMap.get(item);
 
                 for (ItemData itemData : builder.getItemDataList()) {
-                    if (itemData.getSlots().equals(slots)) {
+                    if (itemData.getID() == getIdFor(item)) {
                         return itemData;
                     }
                 }
             }
 
         }
+
 
         return null;
     }
